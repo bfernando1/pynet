cbeeedff6311fad713432c91833a6a814b32ee4b
bfernando1 <bradleylasnames@gmail.com>
2020-10-30 11:51:21 -0700

    Remove python parsing

--- week7/exercise-4.0/ex-4.py
+++ week7/exercise-4.0/ex-4.py
@@ -1,45 +1,33 @@
 #! /usr/bin/env python3
+"""Takes yaml as input and generates a interface config using jinja2 templating
+
+Usage:
+    python3 ex-4.yml
+
+Output:
+    interface Ethernet1
+      switchport mode access
+      switchport access vlan 10
+    interface Ethernet2
+      switchport mode access
+      switchport access vlan 20
+    interface Ethernet3
+      switchport mode trunk
+      switchport trunk native vlan 1
+      switchport trunk allowed vlan al
+
+Author: Bradley Fernando
+"""
 import yaml
 import jinja2
 
 from pprint import pprint
 
 with open("intf.yml") as f:
-    output = yaml.load(f, Loader=yaml.Loader)
-
-# Dictionary of all interfaces
-if_dict = output['interfaces']
-
-
-def if_parse(interface):
-    """Function that creates a generic dictionary for a single interface"""
-    access_vlan_ = None
-    native_vlan_ = None
-    trunk_vlans_ = None
-
-    switchport_mode_ = if_dict[interface]['mode']
-    if switchport_mode_ == 'access':
-        access_vlan_ = if_dict[interface]['vlan']
-    elif switchport_mode_ == 'trunk':
-        native_vlan_ = if_dict[interface]['native_vlan']
-        trunk_vlans_ = if_dict[interface]['trunk_vlan']
+    intf_vars = yaml.load(f, Loader=yaml.Loader)
     
-    if_out = {
-            "interface_name": interface,
-            "switchport_mode": switchport_mode_,
-            "access_vlan": access_vlan_,
-            "native_vlan": native_vlan_,
-            "trunk_vlans": trunk_vlans_
-    }
-    return if_out
-    
+with open("intf.j2") as f:
+    output = f.read()
+t = jinja2.Template(output)
 
-# Write jinja template for each interface
-for k,v in if_dict.items():
-    intf_vars = if_parse(k) 
-
-    with open("intf.j2") as f:
-        output = f.read()
-    t = jinja2.Template(output)
-
-    print(t.render(intf_vars))
+print(t.render(intf_vars))
